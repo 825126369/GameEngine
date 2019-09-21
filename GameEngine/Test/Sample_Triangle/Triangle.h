d424bc07378e002b2b476de4250d4d8c3a11bccb
xuke <825126369@qq.com>
2019-09-21 09:23:10 +0800

    工程整理： 分为 两部分： 一个小游戏 项目，一个各种效果的Demo 项目。 计划： 先实现一个简单的Demo ，把工程基础 搭建起来。

---
+++ GameEngine/Test/Sample_Triangle/Triangle.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Win64.h"
+
+using namespace DirectX;
+using Microsoft::WRL::ComPtr;
+
+class Triangle
+{
+public:
+	Triangle();
+	~Triangle();
+
+	void Init();
+	void LoadPipeline();
+	void LoadAssets();
+
+private:
+	ComPtr<ID3D12Device> mDevice;
+	ComPtr<ID3D12CommandQueue> mCommandQueue;
+	const INT nFrameCount = 3;
+
+};
+
