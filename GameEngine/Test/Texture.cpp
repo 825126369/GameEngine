ba6a10d59e98def89b23e55e6df99a20109e3afe
xuke <825126369@qq.com>
2019-09-25 09:49:01 +0800

    修改

--- GameEngine/Test/Texture.cpp
+++ GameEngine/Test/Texture.cpp
@@ -2,10 +2,47 @@
 
 Texture::Texture()
 {
-
+    
 }
 
 Texture::~Texture()
 {
 
 }
+
+void Texture::SetData(BYTE* data, int32_t Length)
+{
+    this->data = new BYTE[Length];
+    *this->data = *data;
+    dataLength = Length;
+}
+
+int32_t Texture::GetData(BYTE* data)
+{
+    *data = *this->data;
+    return dataLength;
+}
+
+void Texture::SetWidthHeight(int32_t width, int32_t height)
+{
+    this->width = width;
+    this->height = height;
+}
+
+void Texture::GetWidthHeight(int32_t& width, int32_t& height)
+{
+    width = this->width;
+    height = this->height;
+}
+
+// void Texture::SetFormat(int32_t width, int32_t height)
+// {
+//     this->width = width;
+//     this->height = height;
+// }
+
+// void Texture::GetFormat(int32_t& width, int32_t& height)
+// {
+//     width = this->width;
+//     height = this->height;
+// }
--- GameEngine/Test/Texture.h
+++ GameEngine/Test/Texture.h
@@ -9,31 +9,11 @@ class Texture
 public:
 	Texture();
 	~Texture();
-
-	void SetData(BYTE* data, int32_t Length)
-	{
-		this->data = new BYTE[Length];
-		*this->data = *data;
-		dataLength = Length;
-	}
-
-	int32_t GetData(BYTE* data)
-	{
-		*data = *this->data;
-		return dataLength;
-	}
-
-	void SetWidthHeight(int32_t width, int32_t height)
-	{
-		this->width = width;
-		this->height = height;
-	}
-
-	void GetWidthHeight(int32_t& width, int32_t& height)
-	{
-		width = this->width;
-		height = this->height;
-	}
+	
+	void SetData(BYTE* data, int32_t Length);
+	int32_t GetData(BYTE* data);
+	void SetWidthHeight(int32_t width, int32_t height);
+	void GetWidthHeight(int32_t& width, int32_t& height);
 
 private:
 	BYTE* data = nullptr;
